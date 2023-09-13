54c8bfcec7fea17c190f21d18507780a7ae8ccc9
Mayank <mayankbankoti4@gmail.com>
2023-09-13 22:57:58 +0530

    Update

--- Interrupt_LED.c
+++ Interrupt_LED.c
@@ -1,3 +1,25 @@
 #include "LPC214x.h"
 
-#define EXTInt (1<<)
+#define intpin (1<<30)
+#define extint3 (1<<17)
+unsigned int count =0, set =0;
+
+void delay(unsigned int time)
+{
+	unsigned int i,j;
+	for (i=0; i<time; i++)
+	   {
+	   	for (j =0; j <4000; j++);
+	   }
+}
+
+void IRQ_Handler() __irq
+{
+
+}
+
+int main (void)
+{
+
+
+}
